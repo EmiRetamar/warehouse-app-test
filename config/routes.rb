87e491cae89f9476299b25c6abc5600a2ddf08c3
emi.retamar <emiliano_retamar@hotmail.com.ar>
2021-10-17 09:00:29 -0300

    Registro y login terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
+  post 'signin', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
