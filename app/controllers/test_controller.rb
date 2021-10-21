81ecd276a3aa1fc0803faab1163c5c74f452b0ab
Emi Retamar <emiliano_retamar@hotmail.com.ar>
2021-10-21 00:22:08 -0300

    Endpoint para ping

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,11 @@
+class TestController < ApplicationController
+
+  skip_before_action :authorize_request, only: :index
+  skip_before_action :verify_authenticity_token, :only => :index
+
+  def index
+    response = { message: 'OK' }
+    json_response(response, :ok)
+  end
+
+end
