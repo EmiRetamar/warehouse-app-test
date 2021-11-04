0a8da6552b5948e05f132029900fea4cc97062f6
emi.retamar <emiliano_retamar@hotmail.com.ar>
2021-11-04 01:24:56 -0300

    Se regenero la bd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_005230) do
+ActiveRecord::Schema.define(version: 2021_11_04_035251) do
 
   create_table "cash_registers", force: :cascade do |t|
     t.datetime "date"
